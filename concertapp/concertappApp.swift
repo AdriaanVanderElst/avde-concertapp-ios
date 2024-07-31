91283ab89d8b3edb9d9fb58e27a74945438c508a
Adriaan Vander Elst <adriaan.vanderelst.y6760@student.hogent.be>
2024-07-31 10:48:14 +0200

    Initial Commit

---
+++ concertapp/concertappApp.swift
@@ -0,0 +1,17 @@
+//
+//  concertappApp.swift
+//  concertapp
+//
+//  Created by Adriaan Vander Elst on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct concertappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
